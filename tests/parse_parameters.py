fcca51369627e706f4ff663d4d9c8d30926858b5
micafer <micafer1@upv.es>
2023-12-12 16:40:59 +0100

    Remove params files, set tabs in metadata

--- tests/parse_parameters.py
+++ tests/parse_parameters.py
@@ -4,41 +4,33 @@ import io
 import sys
 import os
 import yaml
+import re
 
-parameters_directory = sys.argv[1]
-templates_directory = sys.argv[2]
+templates_directory = sys.argv[1    ]
 
-for path, _, files in os.walk(parameters_directory):
+for path, _, files in os.walk(templates_directory):
     for name in files:
         with io.open(os.path.join(path, name)) as stream:
-            print("Parameters file: " + name)
-            parameters = yaml.full_load(stream)
-
-        ymlf = os.path.join(templates_directory,"%s.yml" % os.path.splitext(name)[0][:-11])
-        yamlf = os.path.join(templates_directory, "%s.yaml" % os.path.splitext(name)[0][:-11])
-        template_file = None
-        print(ymlf)
-        if os.path.exists(ymlf):
-            template_file = ymlf
-        elif os.path.exists(yamlf):
-            template_file = yamlf
-        
-        if template_file is None:
-            print("Template file not found for parameters file: " + name)
-            sys.exit(1)
-
-        with io.open(template_file) as stream:
             print("Template file: " + name)
             template = yaml.full_load(stream)
+            template_inputs = template.get("topology_template", {}).get("inputs", {})
 
-            for key, value in parameters["inputs"].items():
-                if "tab" not in value:
-                    print("Parametes input without tab")
+            tabs = template.get('metadata', {}).get('tabs', {})
+            for tab, input_elems in tabs.items():
+                if not isinstance(tab, str):
+                    raise Exception("Tab name must be a string")
+                # Special case for a regex to select inputs
+                if isinstance(input_elems, str):
+                    all_inputs = list(template_inputs.keys())
+                    res = [elem for elem in all_inputs if re.match(input_elems, elem)]
+                    input_elems = res
+                if not input_elems:
+                    print("Tab %s has no inputs" % tab)
                     sys.exit(1)
-                elif value["tab"] not in parameters["tabs"]:
-                    print("Tab %s not found in parameters file" % value["tab"])
-                    sys.exit(1)
-                if key not in template.get("topology_template", {}).get("inputs", {}):
-                    print("Parameter input %s not found in template" % key)
-                    sys.exit(1)
-
+                for input_elem in input_elems:
+                    input_name = input_elem
+                    if isinstance(input_elem, dict):
+                        input_name = list(input_elem.keys())[0]
+                    if input_name not in template_inputs:
+                        print("Tab input %s not found in template" % input_name)
+                        sys.exit(1)
